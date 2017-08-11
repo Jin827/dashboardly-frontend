c0b8ab0a029d3424585ed03f74c9653314098054
Jin827 <hyojin.lee827@gmail.com>
2017-08-10 23:07:18 -0400

    wrong commit

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  API_HOST: "http://localhost:3000"
-  // API_HOST: "https://private-793ff-dashboardly.apiary-mock.com"
-  // "https://private-anon-c3f982c0e0-dashboardly.apiary-mock.com"
+  API_HOST: "https://private-617a02-dashboardly2.apiary-mock.com"
+  
 }
-// =======
-  //API_HOST:` https://private-anon-3599b87f0d-dashboardly.apiary-mock.com`
 
-// "https://private-anon-c3f982c0e0-dashboardly.apiary-mock.com"
